1a55b269d36eb70bd052a531c603abf6bfaa3218
Romarc Salvador <rmrcslvdr@gmail.com>
2023-06-27 04:39:54 +0800

    Done First Part

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 const ExpenseItem = (props) => {
   //   const expenseDate = new Date(2021, 2, 28);
@@ -9,13 +10,13 @@ const ExpenseItem = (props) => {
   //   const expenseAmount = 280;
 
   return (
-    <div className="expense-item ">
+    <Card className="expense-item ">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price ">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 };
 
